4d181688c8b7e7364c4a3834d2a248ffe7bc6d62
Luke Schoen <ltfschoen@gmail.com>
2017-03-27 22:50:35 +1100

    Refactored

--- binary_search_find_min_sorted_array_rotated.js
+++ binary_search_find_min_sorted_array_rotated.js
@@ -1,25 +1,33 @@
+/**
+ * Given a sorted array as input (ascending order only
+ * and without duplicates) that may be rotated
+ * and return the minimum value.
+ */
+
 let getMin = (x, y) => { return x <= y ? x : y; }
 let getMid = (l, h) => { return parseInt((l + h)/2, 10); }
 
+/** 
+ * Base case of divide and conquer constrains search 
+ * to unsorted sub-array housing the minimum value, 
+ * and iteratively selects and processes the next sub-array
+ * until return answer
+ */
 let findMin = (arr, l, h) => {
-  // constrain search to unsorted sub-array containing min value
   while (arr[l] > arr[h]) {
     let mid = getMid(l, h);
-    // choose next sub-array
     if (arr[mid] > arr[h]) { l = mid + 1 };
     if (arr[mid] <= arr[h]) { h = mid };
   }
-  // answer
   return arr[l];
 }
 
 /**
- *  given a sorted array as input (incrementing order only
- *  and without duplicates) that may be rotated
- *  and return the minimum value.
+ * Call algorithm with sorted and rotated array,
+ * and its lower and upper indexes as arguments
  */
-let input = [2,3,4,-2,-1,0,1], // input
-    l = 0, // lower index
-    h = input.length - 1, // upper index
+let input = [2,3,4,-2,-1,0,1], 
+    l = 0, 
+    h = input.length - 1,
     output = findMin(input, l, h);
 console.log(output);
